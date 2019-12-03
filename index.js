4c5736166704db6e5aec8c10a3a81f70393c00b9
Kevin <kevin@hopemediahouse.com>
2019-12-03 14:44:13 -0800

    exporting AWS service.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const awsService = require('./services/aws.service')
 const logService = require('./services/log.service')
 const knexBaseModel = require('./models/knex_base.model')
+
+exports.initializeAwsService = function (config, logService) {
+  awsService.initialize(config, logService)
+}
 
 exports.initializeLogService = function (app) {
   logService.initialize(app)
@@ -9,6 +14,7 @@ exports.initializeKnexDB = function (database, logService) {
   knexBaseModel.initialize(database, logService)
 }
 
+exports.awsService = require('./services/aws.service')
 exports.errorMessageService = require('./services/error_message.service')
 exports.logService = logService
 exports.responseService = require('./services/response.service')
